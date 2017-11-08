3cc275f557e3097ce2babe8156e1488930231471
Kyle Spindler <kyle.spindler@mntl.co.uk>
2017-11-08 15:34:25 +0000

    added assignment and modified the r script to work

--- Script.R
+++ Script.R
@@ -1,14 +1,18 @@
 library(ggplot2)
 library(foreign)
-nasa <- read.arff("D:\\Projects\\Other\\SoftwareQualityMeasurementAssignment1\\nasa93.arff")
+
+EffortEstimation <- function(x) {12 * (x)}
+ggplot(data.frame(x=c(0,250000)), aes(x=x)) + stat_function(fun = EffortEstimation)
+
+nasa <- read.arff("D:\\Projects\\Other\\SoftwareQualityMeasurementAssignment1\\nasa93.arff.txt")
 #Set the font size to a large font to make the plots more readable
 theme_set(theme_gray(base_size = 18))
 #Visualise the KLOC versus effort as a scatter plot
-qplot(data=nasa,x=equivphyskloc,y=act_effort,size=I(3))
+qplot(data=nasa,x=kloc,y=effort,size=I(3))
 #Add a line representing the linear regression line to the data
-qplot(data=nasa,x=equivphyskloc,y=act_effort,size=I(3))+ geom_smooth(method = "lm", se = FALSE)
+qplot(data=nasa,x=kloc,y=effort,size=I(3))+ geom_smooth(method = "lm", se = FALSE)
 #Calculate the linear model
-fit <- lm(nasa$act_effort~nasa$equivphyskloc)
+fit <- lm(nasa$effort~nasa$kloc)
 #Print out the coefficients of the linear model
 summary(fit)
 #Make your own function, called EffortEstimation, assuming x is the LOC we supply,
